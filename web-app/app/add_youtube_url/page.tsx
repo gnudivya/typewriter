ec81ef1144adc9894019dd515f25a7bd3195044e
Divya <gnudivya@gmail.com>
2023-11-12 05:47:32 +0530

    started working on proxy server

---
+++ web-app/app/add_youtube_url/page.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material"
+
+export default function AddYoutubeUrl () {
+    return <Box>
+        <Typography >
+            Hi...
+        </Typography>
+    </Box>
+}
